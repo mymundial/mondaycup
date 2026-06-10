3193aaa65a34ae56f484ec92be2a2db21c099d8e
mymundial <alexjashworth@gmail.com>
2026-06-10 14:26:31 +0100

    Delete src/src directory

--- src/src/components/profile/ProfileScreens.jsx
+++
@@ -1,3 +0,0 @@
-export { ClubhouseScreen } from "./ClubhouseScreen.jsx";
-export { TrophyCabinetScreen } from "./TrophyCabinetScreen.jsx";
-export { LeaderboardScreen } from "./LeaderboardScreen.jsx";
