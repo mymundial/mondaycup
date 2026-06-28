ab3bbeb9325c66c24a8101f73a39b785dfa11568
mymundial <alexjashworth@gmail.com>
2026-06-28 15:48:32 +0100

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.jsx";
 import { ErrorBoundary } from "./components/common/ErrorBoundary.jsx";
 import "./index.css";
@@ -9,5 +10,6 @@ createRoot(document.getElementById("root")).render(
     <ErrorBoundary onReset={() => window.location.reload()}>
       <App />
     </ErrorBoundary>
+    <Analytics />
   </React.StrictMode>
 );
